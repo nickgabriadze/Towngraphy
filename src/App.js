36716414ea5b59ed9275b82dec7424fd36b69678
Nicka <nickgabriadze@gmail.com>
2022-07-15 10:49:27 +0400

    Added Footer Element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React from 'react';
 import './App.css';
-
+import {Footer} from './components/footer.js'
 function App() {
   return (
     <>
+    
+    <Footer />
+  
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+import './footer.css';
+import 'font-awesome/css/font-awesome.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+
+export function Footer() {
+    return (
+        <>
+
+            <div className="footer-container">
+                
+                <div id="flexable-footer">
+                    <div id="linkedIn-logos">
+
+                        <a id="linkedIn-gigi" href="https://www.linkedin.com/in/gigijishkariani/"  target='_blank'>    
+                            <i class="fa fa-linkedin-square fa-2x" aria-hidden="true">   </i>
+                        </a>
+
+                        <a id="linkedIn-nicka"href="https://www.linkedin.com/in/nickgabriadze/"  target='_blank'>    
+                            <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                        </a>
+                    </div>
+
+                <a id="contact-mail" href="mailto:something@gmail.com">Contact</a>
+                <FontAwesomeIcon icon={faCopyright} />
+                </div>
+
+            </div>
+        </>
+    );
+
+};
