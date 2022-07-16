9db403c8e049bf3f81ff38e50699f342a7f6f947
Nicka <nickgabriadze@gmail.com>
2022-07-16 17:58:44 +0400

    Added Timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 import { Footer } from './components/footer.js';
 import { Keyboard } from './components/keyboard.js';
@@ -6,15 +6,15 @@ import { TextBox } from './components/textBox';
 import { ACTIONS } from './actions.js';
 import { randCity } from './checkOrGive/randomCity.js'
 import { cities } from './cities.js';
-import {checkCity} from './checkOrGive/checkCity.js';
-
+import { checkCity } from './checkOrGive/checkCity.js';
+import { Timerwithscore } from './components/timer.js';
 function reducer(state, { type, char }) {
 
   switch (type) {
 
     case ACTIONS.ADD_LETTER:
       if (char === ' ') {
-        return({
+        return ({
           ...state,
           nextCity: state.nextCity + char,
           upper: true,
@@ -48,11 +48,11 @@ function reducer(state, { type, char }) {
       });
 
     case ACTIONS.ENTER:
-      if(!checkCity(cities, state.nextCity)){
-        return({
+      if (!checkCity(cities, state.nextCity)) {
+        return ({
           ...state,
           message: 'Not In a City List!',
-          nextCity: state.randomCity[state.randomCity.length-2].toUpperCase()
+          nextCity: state.randomCity[state.randomCity.length - 2].toUpperCase()
         })
       }
 
@@ -71,13 +71,41 @@ function App() {
 
   let generatedCity = randCity(cities);
   let nextCity = generatedCity[generatedCity.length - 2].toUpperCase();
-  const [state, dispatch] = useReducer(reducer, { randomCity: generatedCity, nextCity: nextCity, message: '', upper: false,});
+  const [state, dispatch] =
+    useReducer(reducer,
+      { randomCity: generatedCity, nextCity: nextCity, 
+        message: '', upper: false, guessed: 0, started: false });
+
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+
+  const timer = setInterval(() => {
+    clearInterval(timer);
+    if (state.started == true) {
+      if (seconds === 0) {
+        setSeconds(59);
+        setMinutes(minutes - 1);
+      } else {
+        setSeconds(seconds - 1)
+
+      }
+      if (seconds === 0 & minutes === 0) {
+        setSeconds(0);
+        setMinutes(0)
+        state.started = false
+      }
+    }
+  }, 1000)
+  let formattedMinute = minutes > 9 ? minutes : `0${minutes}`
+  let formattedSecond = seconds > 9 ? seconds : `0${seconds}`
+
 
   return (
     <>
-
+      <Timerwithscore minute={formattedMinute} second={formattedSecond} guessed={state.guessed} />
       <div className="container">
         <div>
+
           <TextBox randomCity={state.randomCity} thatLetter={state.nextCity} message={state.message} />
         </div>
         <div id="keyBoard">
