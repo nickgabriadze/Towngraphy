fa88ee7c53bc40522489e1797fe8b23667bd0ebc
Nicka <nickgabriadze@gmail.com>
2022-07-17 19:52:54 +0400

    Changed contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './footer.css';
 import 'font-awesome/css/font-awesome.min.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faCopyright, faEnvelope} from '@fortawesome/free-regular-svg-icons';
 
 export function Footer() {
     return (
@@ -11,18 +11,12 @@ export function Footer() {
             <div className="footer-container">
 
                 <div id="flexable-footer">
-                    <div id="linkedIn-logos">
-
-                        <a id="linkedIn-gigi" href="https://www.linkedin.com/in/gigijishkariani/" target='_blank' rel="noreferrer">
-                            <i className="fa fa-linkedin-square fa-2x" aria-hidden="true">   </i>
-                        </a>
-
-                        <a id="linkedIn-nicka" href="https://www.linkedin.com/in/nickgabriadze/" target='_blank' rel="noreferrer">
-                            <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-                        </a>
+                    <div id="contact">
+                        <a href="mailto:something@gmail.com"> <FontAwesomeIcon id="envelope" icon={faEnvelope}/></a>
+                        <a id="contact-mail" href="mailto:something@gmail.com">Contact</a>
+                        
                     </div>
 
-                    <a id="contact-mail" href="mailto:something@gmail.com">Contact</a>
                     <div id="copyright-logo-with-text">
                         <FontAwesomeIcon id="copyright-logo" icon={faCopyright} />
                         All Rights Reserved
