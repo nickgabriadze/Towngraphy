dcd3800b45197fcd143bee17faee88fe8fb235d6
Nicka <nickgabriadze@gmail.com>
2022-07-22 11:28:57 +0400

    Added Header with animations

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faAngleLeft, faCopyright, faEnvelope, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+
+import {faBars, faAngleLeft, faCopyright, faEnvelope, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 
 export function Header() {
@@ -12,7 +12,7 @@ export function Header() {
 
     const [clicked, setClicked] = useState(false);
 
-    const questionMark = <motion.div layout='position' transition={{
+    const questionMark = <motion.div layout='position' id="q-mark" transition={{
         layout: {
             duration: 0.5,
         },
@@ -28,23 +28,43 @@ export function Header() {
         id="explanation-box"><div id="explanation">Guess as many cities as you can, starting with a letter that is second to last character of the given city</div></motion.div>
 
 
-
-    let unOpened = <div  id="menu-name">
-        <motion.div animate={{ opacity: [0, 0.1, 0.3, 0.5, 0.8, 1] }}
-            whileTap={{ scale: 0.9 }} onClick={() => setOpened(!opened)}><FontAwesomeIcon icon={faBars} id="hamburger-menu" /></motion.div>
-        <motion.p id="website-name" style={clicked ? { 'fontSize': '18px' } : { 'fontSize': '25px' }} animate={{ opacity: [0, 0.1, 0.3, 0.5, 0.8, 1] }}>Website Name</motion.p>
-        <div id="question-mark" onClick={() => setClicked(!clicked)}>{clicked ? afterClicked : questionMark}</div>
-        </div>
-
-    let afterOpen = <motion.div id="after-click" layout animate={{ opacity: [0, 0.1, 0.3, 0.5, 0.8, 1] }}>
-        <motion.div onClick={() => setOpened(!opened)} whileTap={{ scale: 0.9 }}><FontAwesomeIcon icon={faAngleLeft} id="hamburger-menu" /></motion.div>
-        <motion.a id="envelope" href="mailto:something@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></motion.a>
-        <motion.div><FontAwesomeIcon id="copyright-logo" icon={faCopyright} /></motion.div>
+    const unOpened = 
+    <>
+    <motion.div id="header-main">   
+    <motion.div animate={{ rotate: -180 }}
+  transition={{
+    repeat: 1,
+    repeatType: "reverse",
+    duration: 1,
+    rotate: 0
+  }}
+  onClick={() => setOpened(!opened)}><FontAwesomeIcon id="hamburger-menu" icon={faBars} /></motion.div>
+    <motion.div animate={{x: [-200, 0]}}  transition={{ type: "spring", stiffness: 100 }}  id="website-name">Website Name</motion.div>
+    <motion.div animate={{x: [-100, 0]}}  transition={{ type: "spring", stiffness: 100 }}   onClick={() => setClicked(!clicked)}>{clicked ? afterClicked: questionMark}</motion.div>
     </motion.div>
+    </>
+
+    const afterOpen =
+    <>
+
+     <motion.div id="after-open">   
+    <motion.div  animate={{ rotate: 180 }}
+  transition={{
+    repeat: 1,
+    repeatType: "reverse",
+    duration: 1,
+    rotate: 0
+  }} onClick={() => setOpened(!opened)}><FontAwesomeIcon id="hamburger-menu-opened" icon={faAngleLeft} /></motion.div>
+    <motion.div animate={{x: [-700, 0]}}  transition={{ type: "spring", stiffness: 100 }}  id="mail"><FontAwesomeIcon icon={faEnvelope} /></motion.div>
+    <motion.div animate={{x: [-500, 0]}}  transition={{ type: "spring", stiffness: 100 }}><FontAwesomeIcon id="copyright-logo" icon={faCopyright}/></motion.div>
+    </motion.div>
+    </>
+  
     return (
         <>
             <div id="header-box">
-                {opened ? afterOpen : unOpened}
+              
+                {opened ? afterOpen:unOpened}
             </div>
         </>
     )
