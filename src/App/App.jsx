2001607de386bcf7cb95413433c03357a1f1a50d
Nicka <nickgabriadze@gmail.com>
2022-07-24 12:53:49 +0400

    Change a couple of things

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import './App.css';
 import { Footer } from '../Components/Footer/footer.js';
 import { Keyboard } from '../Components/Keyboard/keyboard.js';
-import { TextBox } from '../Components/Textbox/textBox.js';
+import { TextBox } from '../Components/Textbox/textBox.jsx';
 import { randCity } from '../CheckOrGenerate/randomCity.js'
 import { popularCities } from '../Utils/cities.js';
 import { Timerwithscore } from '../Components/Timer/timer.js';
@@ -17,7 +17,7 @@ function App() {
 
   let generatedCity = randCity(popularCities);
   let nextCity = generatedCity[generatedCity.length - 2].toUpperCase();
- 
+  const [width, setWidth] = useState(window.innerWidth)
   const [state, dispatch] =
     useReducer(reducer,
       {
@@ -28,37 +28,46 @@ function App() {
 
   useEffect(() => {
     const myInterval = setInterval(() => {
-
-        dispatch({ type: "START_TIMER"})
-        
-      
+      dispatch({ type: "START_TIMER" })
     }, 1000)
+
     return () => {
       clearInterval(myInterval);
     }
-  },[state.seconds])
+  }, [state.seconds])
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   const popUp = <PopUp guessed={state.guessed} dispatch={dispatch} />
   return (
     <>
-   
-    {state.gameOver? popUp : ''}
 
-    <Header />
-    <div id='score-howTo'>
-      <HowTo id="how-to-box"/>
-      {window.innerWidth > 480 ? <Timerwithscore minute={state.minutes} second={state.seconds} guessed={state.guessed} dispatch={dispatch} /> : ''}
-      </div>  
-      
+      {state.gameOver ? popUp : ''}
+
+      <Header />
+      <div id='score-howTo'>
+        <HowTo id="how-to-box" />
+        {width > 480 ? <Timerwithscore minute={state.minutes} second={state.seconds} guessed={state.guessed} dispatch={dispatch} /> : ''}
+      </div>
+
       <div className="container">
-          {window.innerWidth <= 480? <div id="duplicate-timer" ><Timerwithscore minute={state.minutes} second={state.seconds} guessed={state.guessed} dispatch={dispatch} /> </div> : ''}
-          <TextBox randomCity={state.randomCity} thatLetter={state.nextCity} message={state.message} dispatch={dispatch} />
-      
+        {width <= 480 ? <div id="duplicate-timer" ><Timerwithscore minute={state.minutes} second={state.seconds} guessed={state.guessed} dispatch={dispatch} /> </div> : ''}
+        <TextBox randomCity={state.randomCity} thatLetter={state.nextCity} message={state.message} dispatch={dispatch} />
+
         <div id="keyBrd">
           <Keyboard dispatch={dispatch} />
         </div>
       </div>
-      
+
       <div id='footer'>
         <Footer />
       </div>
