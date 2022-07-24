aaaaff0c86c764118b42fdd5c1588d739f623d53
Nicka <nickgabriadze@gmail.com>
2022-07-24 14:06:33 +0400

    added semicolon

--- src/App/PrivacyPolicy.jsx
+++ src/App/PrivacyPolicy.jsx
@@ -3,7 +3,8 @@ import './privacy.css';
 import {motion} from 'framer-motion';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+
 export function PrivacyPolicy(){
     const [clicked, setClicked] = useState(false)
     return(
