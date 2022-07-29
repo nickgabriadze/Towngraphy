2b46e3376cd38f1767a23e82c83b0e1e93ef3e51
Nicka <nickgabriadze@gmail.com>
2022-07-29 18:51:06 +0400

    Modified design

--- src/Components/GameOverPopUp/popUp.jsx
+++ src/Components/GameOverPopUp/popUp.jsx
@@ -6,7 +6,10 @@ import {motion} from 'framer-motion';
 
 export function PopUp({guessed, dispatch}) {
 
-    let numberOfCities = guessed > 1 ? 'cities/countries': 'city/country'
+    let numberOfCities = guessed > 1 ? 'cities/countries': 'city/country';
+    
+    let number = <p style={{'color': '#c3073f', 'display':'inline-block', 
+                            'paddingRight':'2px', 'paddingLeft':'2px'}}>{guessed}</p>
     const handleRestart = (e) =>{
         if(e.key === 'r'){
             dispatch({type:"RESTART"})
@@ -26,7 +29,7 @@ export function PopUp({guessed, dispatch}) {
         <motion.div initial={{opacity: 0}}
         animate={{opacity:[0.1, 0.3, 0.5, 0.8, 1]}} id="popUp-box">
             <p id="times-up">Time's Up!</p>
-            <h4 id="guessed-number">You managed to guess {guessed} {numberOfCities}</h4>
+            <h4 id="guessed-number">You managed to guess {number} {numberOfCities}</h4>
             <button id='restart' onClick={()=>dispatch({type:"RESTART"})}>Restart</button>
         </motion.div>
         
