7a21ddb80221c2d25cfc9ce6dd621856d53d1b3d
Nicka <nickgabriadze@gmail.com>
2022-08-01 20:14:16 +0400

    changed keyboard button positions

--- src/Components/Keyboard/keyboard.js
+++ src/Components/Keyboard/keyboard.js
@@ -95,9 +95,9 @@ export function Keyboard({ dispatch }) {
                     whileTap={{ scale: 0.9 }}
                     onMouseDown={(e) => e.preventDefault()}
                     onClick={() => dispatch({ type: "ADD_LETTER", char: '-' })}>-</motion.button>
-
-                    <motion.button whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "REMOVE_LETTER" })}><FontAwesomeIcon icon={faDeleteLeft} id='remove-letter'/></motion.button>
+                <motion.button whileHover={{ scale: 1.05 }}
+                    onMouseDown={(e) => e.preventDefault()}
+                    whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "ENTER" })} id="enter-button">Enter</motion.button>                   
                 <motion.button whileHover={{ scale: 1.1 }}
                     onMouseDown={(e) => e.preventDefault()}
                     whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "ADD_LETTER", char: 'z' })}>Z</motion.button>
@@ -119,9 +119,8 @@ export function Keyboard({ dispatch }) {
                 <motion.button whileHover={{ scale: 1.1 }}
                     onMouseDown={(e) => e.preventDefault()}
                     whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "ADD_LETTER", char: 'm' })}>M</motion.button>
-                <motion.button whileHover={{ scale: 1.05 }}
-                    onMouseDown={(e) => e.preventDefault()}
-                    whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "ENTER" })} id="enter-button">Enter</motion.button>
+               <motion.button whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }} onClick={() => dispatch({ type: "REMOVE_LETTER" })}><FontAwesomeIcon icon={faDeleteLeft} id='remove-letter'/></motion.button>
 
                 <motion.button whileHover={{ scale: 1.02 }}
                     onMouseDown={(e) => e.preventDefault()}
